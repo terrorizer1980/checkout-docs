71a1ac44bfd517b46219f3e4cafda724f7b72ada
bluepnume <chuntheunavoidable@gmail.com>
2020-12-02 06:34:32 +0000

    Deploying to gh-pages from  @ dce20ff3a7079b82e474fc450fd92729dba364fd 🚀

--- _next/static/zmDvhjuIYvqHt3USPgRQ8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F35cc20f11fad851c038a.css","static\u002Fchunks\u002Fpages\u002Findex-a598a9cf8409a912ab95.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-63e5ef04f821c20d5805.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
