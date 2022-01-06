b95f518639ac0abe360befd3849feaf950974a64
gregjopa <534034+gregjopa@users.noreply.github.com>
2022-01-06 19:20:10 +0000

    Deploying to gh-pages from  @ 2871ebd1387e5fe6d657cd42b63119fa274a8609 🚀

---
+++ _next/static/pQGMutDYyHIYuwOjfklVF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ab24eb7bfb464ebec7f4.css","static/chunks/pages/index-34ead585cc8edf38ec99.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
